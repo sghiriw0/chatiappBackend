11ad01c33546e9101d696ff80880f66e63820851
sghiri <sghiriw@gmail.com>
2025-02-22 12:59:46 +0800

    update second commit
    updated server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ socketServer.registerSocketServer(server);
 
 // console.log(process.env.MONGO_URI);
 
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -25,4 +26,8 @@ mongoose
   .catch((err) => {
     console.log("database connection failed. Server not started");
     console.log(err);
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Backend is Running ");
   });
